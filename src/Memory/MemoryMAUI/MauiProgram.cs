b82ff5be2229f9c1ee5e10fbe0e1aa90dd708c00
Sami HALILOU <sahalilou>
2025-03-19 17:07:54 +0100

    added Community toolkit MAUI in project

--- src/Memory/MemoryMAUI/MauiProgram.cs
+++ src/Memory/MemoryMAUI/MauiProgram.cs
@@ -1,5 +1,30 @@
 ﻿using Microsoft.Extensions.Logging;
 
+using CommunityToolkit.Maui;
+using MemoryMAUI;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+            .UseMauiCommunityToolkit()
+            // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // Continue initializing your .NET MAUI App here
+
+        return builder.Build();
+    }
+}
+
 namespace MemoryMAUI
 {
     public static class MauiProgram
