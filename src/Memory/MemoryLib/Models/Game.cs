246e314ac40df4486420c35bf5bb1001fdba8321
Ghassan JABBOUR <ghjabbour>
2025-04-11 17:32:35 +0200

    work on game class

--- src/Memory/MemoryLib/Models/Game.cs
+++ src/Memory/MemoryLib/Models/Game.cs
@@ -2,6 +2,29 @@
 {
     public class Game
     {
+        private Player Player1 { get; set; } = new Player();
+        private Player Player2 { get; set; } = new Player();
+        private Grid Grid { get; set; } = new Grid();
 
+        internal Player CurrentPlayer = null;
+
+        private int Round;
+
+        int RemainingCardsCount { get; set; }
+
+        public void SwitchPlayer()
+        {
+            if (CurrentPlayer == Player1) CurrentPlayer = Player2;
+            else CurrentPlayer = Player1;
+        }
+
+        public void StartGame()
+        {
+            Round = 1;
+            Grid = new Grid();
+            CurrentPlayer = Player1;
+        }
+
+        public bool IsGameOver() { return RemainingCardsCount == 0; }
     }
 }
