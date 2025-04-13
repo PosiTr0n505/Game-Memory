e96ad50f776c017b6023faec23ba5beddfca2e16
Sami213 <samibg63100@gmail.com>
2025-04-13 19:19:27 +0200

    adding tests after merging with th test branch

---
+++ src/Memory/Tests/PlayerTest.cs
@@ -0,0 +1,17 @@
+﻿using MemoryLib.Models;
+
+namespace Tests
+{
+    public class PlayerTest
+    {
+        [Fact]
+        public void Add_1_To_CurrentScore_Result_Is_One()
+        {
+            Player p = new Player("");
+
+            p.add1ToMovesCount();
+
+            Assert.Equal(1, p.MovesCount);
+        }
+    }
+}
