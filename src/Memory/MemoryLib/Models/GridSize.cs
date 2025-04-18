d75a4d3fed64d45545342eed05114664887d2452
Sami HALILOU <sahalilou>
2025-04-18 16:22:30 +0200

    add grid size enum

---
+++ src/Memory/MemoryLib/Models/GridSize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryLib.Models
+{
+    public enum GridSize
+    {
+        g4x4,
+        g4x5, 
+        g4x6, 
+        g6x5, 
+        g6x7, 
+        g6x8
+    }
+}
