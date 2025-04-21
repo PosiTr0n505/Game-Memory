f3dd9acbe906c0bf860c4f9a6bfb9545177df522
Ghassan Jabbour <ghassj14@gmail.com>
2025-04-21 15:33:53 +0200

    modify tests for Game.cs

--- src/Memory/MemoryLib/Models/Game.cs
+++ src/Memory/MemoryLib/Models/Game.cs
@@ -2,28 +2,28 @@
 {
     public class Game
     {
+        public Player? Player1 { get; }
+        public Player? Player2 { get; }
+        public Player? CurrentPlayer { get; set; }
+
+        public Grid? Grid { get; set; } = new Grid();
+
+        private readonly int Round = 1;
+
+        int RemainingCardsCount { get; set; }
+
         public Game(Player? player1, Player? player2)
         {
             Player1 = player1;
             Player2 = player2;
             RemainingCardsCount = 0;
+            CurrentPlayer = player1;
         }
-
-        public Player? Player1 { get; init; }
-        public Player? Player2 { get; init; }
-
-        public Grid? Grid { get; set; } = new Grid();
-
-        public Player? CurrentPlayer { get; private set; }
-
-        private int Round = 1;
-
-        int RemainingCardsCount { get; set; }
 
         public void SwitchPlayer()
         {
-            if (ReferenceEquals(CurrentPlayer, Player1)) CurrentPlayer = Player2;
-            else CurrentPlayer = Player1;
+            if (CurrentPlayer == Player1) { CurrentPlayer = Player2; }
+            else { CurrentPlayer = Player1; }
         }
 
         public bool IsGameOver() { return RemainingCardsCount == 0; }
---
+++ src/Memory/Tests/GameTest.cs
@@ -0,0 +1,29 @@
+﻿using MemoryLib.Models;
+
+namespace Tests
+{
+    public class GameTest
+    {
+        [Fact]
+        public void Switch_player_1_to_player_2()
+        {
+            Player player1 = new("Player 1");
+            Player player2 = new("Player 2");
+            Game? game = new(player1, player2);
+            game.CurrentPlayer = player1;
+            game.SwitchPlayer();
+            Assert.Equal(player2, game.CurrentPlayer);
+        }
+
+        [Fact]
+        public void Switch_player_2_to_player_1()
+        {
+            Player player1 = new("Player 1");
+            Player player2 = new("Player 2");
+            Game? game = new(player1, player2);
+            game.CurrentPlayer = player2;
+            game.SwitchPlayer();
+            Assert.Equal(player1, game.CurrentPlayer);
+        }
+    }
+}
