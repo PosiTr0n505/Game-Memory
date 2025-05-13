c9cd3a2e69e55d323d7be25097d6bdced96bd117
Sami HALILOU <sahalilou>
2025-05-13 09:36:02 +0200

    add ConsoleAsking class for user's entry

--- src/Memory/MemoryConsole/Program.cs
+++ src/Memory/MemoryConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using MemoryConsole;
 using MemoryLib.Managers;
 using MemoryLib.Models;
 
@@ -65,75 +66,16 @@ namespace MyApp
 
         static void StartTwoPlayersGame()
         {
+            ConsoleAsking consoleAsk = new();
             Clear();
             WriteLine("Starting Two Players Game...\nEnter The Name of the 2 Players\n");
 
-            AskTwoPlayersNames(out string? P1Name, out string? P2Name);
+            consoleAsk.AskTwoPlayersNames(out string? P1Name, out string? P2Name);
             WriteLine();
-            GridSize gridSize = AskGridSize();
+            GridSize gridSize = consoleAsk.AskGridSize();
 
             GameManager gameManager = new GameManager(new Game(new Player(P1Name), new Player(P2Name), gridSize));
             gameManager.StartGame();
-        }
-
-        static void AskTwoPlayersNames(out string? P1Name, out string? P2Name)
-        {
-            Write("Player 1 :   ");
-            P1Name = ReadLine();
-            while (string.IsNullOrWhiteSpace(P1Name))
-            {
-                Write("Please enter a valid name.\nPlayer 1 :   ");
-                P1Name = ReadLine();
-            }
-
-            Write("Player 2 :   ");
-            P2Name = ReadLine();
-
-            if (P2Name == P1Name)
-            {
-                WriteLine("The two players cannot have the same name.\n");
-                P2Name = null;
-            }
-
-            while (string.IsNullOrWhiteSpace(P2Name))
-            {
-                Write("Please enter a valid name.\nPlayer 2 :   ");
-                P2Name = ReadLine();
-            }
-        }
-
-        static GridSize AskGridSize()
-        {
-            GridSizeManager gridSizeManager = new GridSizeManager();
-            WriteLine("Select the grid size:");
-            int i=1;
-            foreach (var size in gridSizeManager.Values)
-            {
-                WriteLine($"{i} : {size.Value}");
-                ++i;
-            }
-            Write("\nGrid Size : ");
-            string? choice = ReadLine();
-
-            int gsc = gridSizeManager.Values.Count;
-
-            int gridSize;
-
-            while (!int.TryParse(choice, out gridSize) || gridSize < 1 || gridSize > gsc)
-            {
-                if (!int.TryParse(choice, out gridSize))
-                {
-                    WriteLine("Invalid input. Please enter a number.");
-                }
-                else if (gridSize < 1 || gridSize > gsc)
-                {
-                    WriteLine($"Invalid choice. Please select a number between 1 and {gsc}.");
-                }
-                Write("Select the grid size: ");
-                choice = ReadLine();
-            }
-
-            return (GridSize)(gridSize - 1);
         }
 
         static void ShowGameRules()
