f401b81eb5fca4cbcc613ff938f5615f831e8afd
Sami HALILOU <sahalilou>
2025-05-14 12:06:42 +0200

    add event

---
+++ src/Memory/MemoryConsole/Display/ScoreWriter.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+using MemoryLib.Models;
+
+
+namespace MemoryConsole.Display
+{
+    internal class ScoreWriter
+    {
+        public void OnScoreChange(Player player, int score)
+        {
+            Write($"{player.NameTag} : {score}");
+        }
+    }
+}
