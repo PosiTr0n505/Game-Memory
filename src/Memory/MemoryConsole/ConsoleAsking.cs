e9895ce7204ef060994f43cb357b99d9951548f4
Sami213 <samibg63100@gmail.com>
2025-05-14 22:22:54 +0200

    prepare merge

--- src/Memory/MemoryConsole/ConsoleAsking.cs
+++ src/Memory/MemoryConsole/ConsoleAsking.cs
@@ -45,39 +45,5 @@ namespace MemoryConsole
                 P2Name = ReadLine();
             }
         }
-
-        public GridSize AskGridSize()
-        {
-            GridSizeManager gridSizeManager = new GridSizeManager();
-            WriteLine("Select the grid size:");
-            int i = 1;
-            foreach (var size in gridSizeManager.Values)
-            {
-                WriteLine($"{i} : {size.Value}");
-                ++i;
-            }
-            Write("\nGrid Size : ");
-            string? choice = ReadLine();
-
-            int gsc = gridSizeManager.Values.Count;
-
-            int gridSize;
-
-            while (!int.TryParse(choice, out gridSize) || gridSize < 1 || gridSize > gsc)
-            {
-                if (!int.TryParse(choice, out gridSize))
-                {
-                    WriteLine("Invalid input. Please enter a number.");
-                }
-                else if (gridSize < 1 || gridSize > gsc)
-                {
-                    WriteLine($"Invalid choice. Please select a number between 1 and {gsc}.");
-                }
-                Write("Select the grid size: ");
-                choice = ReadLine();
-            }
-
-            return (GridSize)(gridSize - 1);
-        }
     }
 }
