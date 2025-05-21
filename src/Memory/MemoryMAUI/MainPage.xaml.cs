53b6e0a5dea7635029a88e582afc747b7ad4cf24
Mylan PERRIER <myperrier>
2025-05-21 15:00:19 +0200

    WIP:114-add navigation in home page buttons + doc in appshell

--- src/Memory/MemoryMAUI/MainPage.xaml.cs
+++ src/Memory/MemoryMAUI/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MemoryMAUI
+﻿using MemoryMAUI.Pages;
+
+namespace MemoryMAUI
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,38 @@
         {
             InitializeComponent();
         }
+
+        private void NavigateToSingleplayerPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SingleplayerPage());
+        }
+        private void NavigateToTwoplayersPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TwoPlayersPage());
+        }
+        private void NavigateToGamerulesPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MemoryRulesPage());
+        }
+        private void NavigateToLeaderboardPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LeaderboardPage());
+        }
+        private void NavigateToCreditsPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CreditsPage());
+        }
+        private void QuitButton_Clicked(object sender, EventArgs e)
+        {
+        #if ANDROID
+            Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
+        #elif WINDOWS
+            System.Environment.Exit(0);
+        #else
+            Application.Current.Quit();
+        #endif
+        }
+
     }
 
 }
