38b1df1e96e5548d67ff33e374f06f5891a25e40
Ghassan JABBOUR <ghjabbour>
2025-05-21 15:23:19 +0200

    WIP: databinding

--- src/Memory/MemoryMAUI/MainPage.xaml.cs
+++ src/Memory/MemoryMAUI/MainPage.xaml.cs
@@ -1,11 +1,22 @@
-﻿namespace MemoryMAUI
+﻿using System.Security.Cryptography.X509Certificates;
+using MemoryLib.Models;
+
+namespace MemoryMAUI
 {
     public partial class MainPage : ContentPage
     {
 
+        public Player player1 { get; private set; } = new Player ""player1"";
         public MainPage()
         {
+           
+
             InitializeComponent();
+        }
+
+        void Start_game_button(System.Object Sender, EventArgs e)
+        {
+
         }
     }
 
