ff9300b5f48ff9443625b1fb111c401781674ad5
Sami HALILOU <sahalilou>
2025-05-22 11:58:13 +0200

    LeaderboardPage display leaderboard with STUB

--- src/Memory/MemoryMAUI/Pages/LeaderboardPage.xaml.cs
+++ src/Memory/MemoryMAUI/Pages/LeaderboardPage.xaml.cs
@@ -1,9 +1,18 @@
+using MemoryLib.Models;
+using Persistence;
+
 namespace MemoryMAUI.Pages;
 
 public partial class LeaderboardPage : ContentPage
 {
+	private readonly Leaderboard leaderboard = new(new StubLoadManager(), new StubSaveManager());
+
+	public List<Score> Scores {  get; private set; }
+
 	public LeaderboardPage()
 	{
 		InitializeComponent();
-	}
+		Scores = [.. leaderboard.Scores];
+        BindingContext = this;
+    }
 }
--- src/Memory/MemoryMAUI/Resources/Views/LeadeboardCell.xaml.cs
+++ src/Memory/MemoryMAUI/Resources/Views/LeadeboardCell.xaml.cs
@@ -7,15 +7,9 @@ using MemoryLib.Models;
 
 public partial class LeadeboardCell : ContentView
 {
-    private readonly Score? score;
-
-    private readonly GridSizeToString converter = new();
-
     public LeadeboardCell()
 	{
 		InitializeComponent();
-
-        gridSizeLabel.Text = (string)converter.Convert(score!.GridSize, typeof(string), null, CultureInfo.CurrentCulture)!;
 	}
 
 
