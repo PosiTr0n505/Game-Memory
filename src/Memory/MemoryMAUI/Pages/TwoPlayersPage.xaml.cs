459d7e4a36cf91a79a29d12bdfb43d0846f03b58
Ghassan Jabbour <ghassj14@gmail.com>
2025-05-27 00:12:42 +0200

    WIP

--- src/Memory/MemoryMAUI/Pages/TwoPlayersPage.xaml.cs
+++ src/Memory/MemoryMAUI/Pages/TwoPlayersPage.xaml.cs
@@ -1,9 +1,17 @@
+using MemoryLib.Models;
+
 namespace MemoryMAUI.Pages;
 
 public partial class TwoPlayersPage : ContentPage
 {
-	public TwoPlayersPage()
-	{
-		InitializeComponent();
-	}
+    public Player Player1 { get; private set; }
+    public Player Player2 { get; private set; }
+
+    public TwoPlayersPage()
+    {
+        InitializeComponent();
+        Player1 = new Player("Player 1");
+        Player2 = new Player("Player 2");
+        BindingContext = this;
+    }
 }
