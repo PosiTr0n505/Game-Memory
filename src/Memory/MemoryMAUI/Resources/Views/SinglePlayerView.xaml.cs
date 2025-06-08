214cd3afb9fab04e657fa02f68803dc7937c9298
Sami213 <samibg63100@gmail.com>
2025-06-08 23:52:28 +0200

    finish

--- src/Memory/MemoryMAUI/Resources/Views/SinglePlayerView.xaml.cs
+++ src/Memory/MemoryMAUI/Resources/Views/SinglePlayerView.xaml.cs
@@ -1,5 +1,6 @@
 using MemoryLib.Managers;
 using MemoryLib.Models;
+using MemoryMAUI.Converters;
 using System.Diagnostics;
 
 namespace MemoryMAUI.Resources.Views;
@@ -61,4 +62,69 @@ public partial class SinglePlayerView : ContentView
         await Shell.Current.GoToAsync("singleplayergamepage", navigationParameter);
 
     }
+
+    private void EldenRingButtonClicked(object sender, EventArgs e)
+    {
+        CardBooleanToImageSource.SelectTheme("eldenring");
+    }
+
+    private void PokemonButtonClicked(object sender, EventArgs e)
+    {
+        CardBooleanToImageSource.SelectTheme("pokemon");
+    }
+
+    private async void CustomThemeButtonClicked(object sender, EventArgs e)
+    {
+        if (!Directory.Exists(Path.Combine(AppContext.BaseDirectory, "Images")))
+            Directory.CreateDirectory(Path.Combine(AppContext.BaseDirectory, "Images"));
+
+        var (hiddenCard, visibleCard) = CardBooleanToImageSource.Themes.GetValueOrDefault("custom");
+
+        string? sourceHiddenCard = hiddenCard?.ToString()?.Replace("File:", "", StringComparison.OrdinalIgnoreCase)?.Trim();
+        string? sourceVisibleCard = visibleCard?.ToString()?.Replace("File:", "", StringComparison.OrdinalIgnoreCase)?.Trim();
+
+        if (string.IsNullOrWhiteSpace(sourceHiddenCard) || string.IsNullOrWhiteSpace(sourceVisibleCard))
+            return;
+
+        string imagesDir = Path.Combine(AppContext.BaseDirectory, "Images");
+
+        string[] allowedExtensions = [".png", ".jpg", ".jpeg"];
+
+        string? hiddenCardPath = allowedExtensions
+            .Select(ext => Path.Combine(imagesDir, sourceHiddenCard + ext))
+            .FirstOrDefault(File.Exists);
+
+        string? visibleCardPath = allowedExtensions
+            .Select(ext => Path.Combine(imagesDir, sourceVisibleCard + ext))
+            .FirstOrDefault(File.Exists);
+
+        if (hiddenCardPath is null || visibleCardPath is null)
+        {
+
+#if WINDOWS
+            Process.Start("explorer.exe", imagesDir);
+#elif MACCATALYST || MACOS
+        Process.Start("open", imagesDir);
+#elif LINUX
+        Process.Start("xdg-open", imagesDir);
+#endif
+
+            var mainPage = Application.Current?.Windows[0].Page;
+
+            if (mainPage is null)
+                return;
+
+            await mainPage.DisplayAlert(
+                "Images not found",
+                "You must place two image files in the 'Images' folder with supported extensions (.png, .jpg, .jpeg). " +
+                "Expected filenames: customhiddencard and customvisiblecard.",
+                "OK"
+            );
+
+            return;
+        }
+
+        CardBooleanToImageSource.SelectTheme("custom", hiddenCardPath, visibleCardPath);
+    }
+
 }
